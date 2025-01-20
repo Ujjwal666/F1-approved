fd2caae4b04864b25e6d3389a70c1fd8ad567be0
Ujjwal666 <66254824+Ujjwal666@users.noreply.github.com>
2025-01-20 11:56:15 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Components/Home/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+        <Home />
+    </div>
+  );
+}
+
+export default App;
